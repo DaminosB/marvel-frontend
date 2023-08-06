b84271af1bf86f4adc0ef2b0cacf74c78180c084
Damien Bourcheix <damien@mbp-de-damien.home>
2023-08-06 21:35:45 +0200

    Marvel logo added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import "./Header.css";
 import { useNavigate, Link } from "react-router-dom";
 import Cookies from "js-cookie";
-
+import logo from "../../assets/img/logo.png";
 const Header = ({ token, setToken, setShowConnexion }) => {
   const navigate = useNavigate();
   return (
     <header className="container">
+      <img src={logo} alt="logo Marvel" />
       <div>
         <Link to="/">
           <h1>MARVEL.database</h1>
